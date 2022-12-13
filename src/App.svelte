4218cbf8224d31f3cd939feae0c3cb2c23225def
Gustavin-Rocha <85358011+Gustavin-Rocha@users.noreply.github.com>
2022-12-13 13:23:35 -0300

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import Calculadora from "./components/Calculadora.svelte";
+</script>
+
+<main>
+	<Calculadora />
+</main>
+
+<style>
+	:global(body) {
+		background-image: url('../vetores/math.jpg');
+	}
+	
+	:root {
+		--cor-fundo: rgb(0, 0, 0);
+	}
+
+	main {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 90vh;
+	}
+</style>
---
+++ src/components/Botao.svelte
@@ -0,0 +1,44 @@
+<script lang="ts"> //Pode se usar TypeScript.
+    export let texto: string
+    export let triplo: boolean = false
+    export let duplo: boolean = false
+    export let operacao: boolean = false
+    export let destaque: boolean = false
+    export let onClick: (valor: string) => void = () => {}
+
+    function click() {
+        console.log('click...')
+    }
+</script>
+
+<button class="botao" 
+    on:click={() => onClick(texto)}
+    class:triplo class:duplo class:operacao class:destaque>
+    {texto}
+</button>
+
+<style>
+    .botao {
+        flex-basis: 25%;
+        margin: 0;
+        border: 1px solid var(--cor-fundo);
+    }
+
+    .triplo {
+        flex-basis: 75%;
+    }
+
+    .duplo {
+        flex-basis: 50%;
+    }
+
+    .operacao {
+        background-color: rgb(0, 140, 255);
+        color: white;
+    }
+
+    .destaque {
+        background-color: rgb(255, 0, 0);
+        color: white;
+    }
+</style>
